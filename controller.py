44c59cf5a0a159687d686fe53518b37bcb75a6a2
LECHIY94 <122964071+LECHIY94@users.noreply.github.com>
2023-05-24 11:36:01 +0300

    Работаем

--- controller.py
+++ controller.py
@@ -6,23 +6,17 @@ def start():
         view.menu()
         answer = input('Введите команду\n')
         if answer == '1':
-            view.show()
+            name = input('Введите фамилию контакта:\n') 
+            phone = input('Введите телефон контакта:\n')
+            model.add_contact(name, phone: str)
         
         elif answer == '2':
-            name = input('Введите фамилию контакта:\n') # может сделать отдельно ФИО, отельно телефон?
-            phone = input('Введите телефон контакта:\n')
-            model.add_contact(name, phone)
+            data = input('Введите данные контакта через пробел\n')
+            model.find_contact(data: str, query)       
         
         elif answer == '3':
-            data = input('Введите данные контакта через пробел\n')
-            model.search(data)       
-        
-        elif answer == '4':
-            view.bye()
-            break
-        
-        else:
-            model.error() 
+            name = input('Введите имя или фамилию контакта, который хотите удалить:  ')
+            view.delete_contact(data: str, query)
             
 if __name__ == '__main__':
     start()  
