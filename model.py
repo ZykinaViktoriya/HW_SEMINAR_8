95e7138967c0b29e754974b43c9a2f0e750aa8b7
LECHIY94 <122964071+LECHIY94@users.noreply.github.com>
2023-05-24 11:36:58 +0300

    Работаем

--- model.py
+++ model.py
@@ -2,27 +2,14 @@ def add_contact(name, phone: str):
     with open('file.txt', 'a', encoding='utf-8') as file:
         file.write(f'{name} - {phone}\n') 
         
-        
-# if __name__ == '__main__':
-#     add_contact('Hello world')
-    
-    
-def read_file():
-    with open('file.txt', 'a', encoding='utf-8') as file:
-        return file.read().split('\n')[: -1]
-    
-
-def delete_contact(data: str, query):
-    query = input('Введите имя или фамилию контакта, который хотите удалить:  ')
-
-    with open('file.txt', 'r', encoding='UTF-8') as file:
-        for i in range(0, len(data)):
-            if query == data[i]['Name'] or query == data[i]['Surname']:
-                del data[i]
-
 def find_contact(data: str, query):
-    query = input('Введите имя контакта: ')
         with open('file.txt') as file:
             for line in file:
                 if (data.last_name, data.name) == query:
                     return data
+    
+def delete_contact(data: str, query):
+    with open('file.txt', 'r', encoding='UTF-8') as file:
+        for i in range(0, len(data)):
+            if name == data[i]['Name'] or name == data[i]['Surname']:
+                del data[i]
