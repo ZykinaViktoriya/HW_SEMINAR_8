7569cac69285650a368ead384458cbc15be049ff
LECHIY94 <122964071+LECHIY94@users.noreply.github.com>
2023-05-25 15:35:03 +0300

    Конечный вариант

--- controller.py
+++ controller.py
@@ -2,21 +2,25 @@ import view, model
 
 def start():
     view.greetings()
-    while True:
-        view.menu()
-        answer = input('Введите команду\n')
-        if answer == '1':
-            name = input('Введите фамилию контакта:\n') 
-            phone = input('Введите телефон контакта:\n')
-            model.add_contact(name, phone: str)
-        
-        elif answer == '2':
-            data = input('Введите данные контакта через пробел\n')
-            model.find_contact(data: str, query)       
-        
-        elif answer == '3':
-            name = input('Введите имя или фамилию контакта, который хотите удалить:  ')
-            view.delete_contact(data: str, query)
-            
-if __name__ == '__main__':
-    start()  
+
+while True:
+    start()
+    view.menu()
+    
+   
+    choice = input("Введите номер действия: ")
+
+    if choice == "1":
+        name = input("Введите имя контакта: ")
+        phone_number = input("Введите номер телефона: ")
+        model.add_contact(name, phone_number)
+    elif choice == "2":
+        name = input("Введите имя контакта: ")
+        model.remove_contact(name)
+    elif choice == "3":
+        name = input("Введите имя контакта: ")
+        model.find_contact(name)
+    elif choice == "4":
+        break
+    
+    
