d0d0ff66c086f7e0628ff78ca7f4e3a4be807b0c
LECHIY94 <122964071+LECHIY94@users.noreply.github.com>
2023-05-25 15:42:07 +0300

    Конечный вариант

--- model.py
+++ model.py
@@ -1,15 +1,27 @@
-def add_contact(name, phone: str):
-    with open('file.txt', 'a', encoding='utf-8') as file:
-        file.write(f'{name} - {phone}\n') 
+phone_book = {}
+
+def get_data():
+    with open('phone_book.txt','r') as file:
+        for key,val in phone_book.items():
+            phone_book = file.write('{}:{}\n'.format(key,val))
+    return phone_book                
+
+def add_contact(name, phone_number):
+    with open('phone_book.txt', 'a', encoding='utf-8') as file:
+        phone_book[name] = phone_number
+        print(f"Контакт {name} успешно добавлен")
         
-def find_contact(data: str, query):
-        with open('file.txt') as file:
-            for line in file:
-                if (data.last_name, data.name) == query:
-                    return data
-    
-def delete_contact(data: str, query):
-    with open('file.txt', 'r', encoding='UTF-8') as file:
-        for i in range(0, len(data)):
-            if name == data[i]['Name'] or name == data[i]['Surname']:
-                del data[i]
+def remove_contact(name):
+    with open('phone_book.txt', 'r', encoding='utf-8') as file:
+        if name in phone_book:
+            del phone_book[name]
+            print(f"Контакт {name} успешно удален")
+        else:
+            print(f"Контакт {name} не найден")
+
+def find_contact(name):
+    with open('phone_book.txt', 'r', encoding='utf-8') as file:
+        if name in phone_book:
+            print(f"Номер телефона {name}: {phone_book[name]}")
+        else:
+            print(f"Контакт {name} не найден")
