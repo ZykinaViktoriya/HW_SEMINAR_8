8366fbdff8b7fa9b2dc516deb24fb4dbb059369d
Mari <r.m.u@bk.ru>
2023-05-26 18:21:25 +0300

    Последние доработки

--- model.py
+++ model.py
@@ -10,3 +10,15 @@ def find_contact(name):
         for i in file:
             if name in i:
                 print (i)
+
+def remove_contact(name):
+    import re
+    with open('phone_book.txt', 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    pattern = re.compile(re.escape(name))
+    with open('phone_book.txt', 'w', encoding='utf-8' ) as file:
+        for line in lines:
+            result = pattern.search(line)
+            if result is None:
+                file.write(line)
+        print(f"Контакт {name} успешно удален\n")
