5911cc5581beb295aa4edeccc150d4d72d0557af
Виктория <121007951+ZykinaViktoriya@users.noreply.github.com>
2023-05-26 17:58:48 +0300

    Update model.py
    created fuction for model

--- model.py
+++ model.py
@@ -1,27 +1,12 @@
-phone_book = {}
-
-def get_data():
-    with open('phone_book.txt','r') as file:
-        for key,val in phone_book.items():
-            phone_book = file.write('{}:{}\n'.format(key,val))
-    return phone_book                
+import view           
 
 def add_contact(name, phone_number):
     with open('phone_book.txt', 'a', encoding='utf-8') as file:
-        phone_book[name] = phone_number
-        print(f"Контакт {name} успешно добавлен")
+        file.write('{}:{}\n'.format(name,phone_number))
+        print(f"Контакт {name} успешно добавлен\n")
         
-def remove_contact(name):
-    with open('phone_book.txt', 'r', encoding='utf-8') as file:
-        if name in phone_book:
-            del phone_book[name]
-            print(f"Контакт {name} успешно удален")
-        else:
-            print(f"Контакт {name} не найден")
-
 def find_contact(name):
-    with open('phone_book.txt', 'r', encoding='utf-8') as file:
-        if name in phone_book:
-            print(f"Номер телефона {name}: {phone_book[name]}")
-        else:
-            print(f"Контакт {name} не найден")
+    with open('phone_book.txt', 'r', encoding='utf-8' ) as file:
+        for i in file:
+            if name in i:
+                print (i)
