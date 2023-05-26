4341ebc741c2b80ce46b58ce340c97db5f69a4a2
LECHIY94 <122964071+LECHIY94@users.noreply.github.com>
2023-05-26 17:44:36 +0300

    Finish

--- controller.py
+++ controller.py
@@ -2,25 +2,25 @@ import view, model
 
 def start():
     view.greetings()
+    
 
-while True:
-    start()
-    view.menu()
+    while True:
+        
+        
+        view.menu()
+        
     
-   
-    choice = input("Введите номер действия: ")
+        choice = input("Введите номер действия: ")
 
-    if choice == "1":
-        name = input("Введите имя контакта: ")
-        phone_number = input("Введите номер телефона: ")
-        model.add_contact(name, phone_number)
-    elif choice == "2":
-        name = input("Введите имя контакта: ")
-        model.remove_contact(name)
-    elif choice == "3":
-        name = input("Введите имя контакта: ")
-        model.find_contact(name)
-    elif choice == "4":
-        break
-    
-    
+        if choice == "1":
+            name = input("Введите имя контакта: ")
+            phone_number = input("Введите номер телефона: ")
+            model.add_contact(name, phone_number)
+        elif choice == "2":
+            name = input("Введите имя контакта: ")
+            model.remove_contact(name)
+        elif choice == "3":
+            name = input("Введите имя контакта: ")
+            model.find_contact(name)
+        elif choice == "4":
+            break
